11101418cf525cdf89e1b4938c5c184ca4085f7b
Paula García <paulaagarcia1@gmail.com>
2018-01-02 23:53:01 -0500

    Functions

--- First Exercises/Test1.js
+++ First Exercises/Test1.js
@@ -4,5 +4,26 @@ var money = 20; //how much money
 if (money>=price) {
   console.log("Buy the hammer");
 } else {
-  console.log("Don't buy the hammer");  
+  console.log("Don't buy the hammer");
+}
+
+var todos  = ["item 1","item 2","item 3"];
+
+function displayTodos() {
+  console.log("My todos:", todos);
+}
+
+function addTodo(todo) {
+  todos.push(todo);
+  displayTodos();
+}
+
+function  changeTodo(position, newValue) {
+  todos[position] = newValue;
+  displayTodos();
+}
+
+function deleteTodo (position) {
+  todos.splice(position,1);
+  displayTodos;
 }
